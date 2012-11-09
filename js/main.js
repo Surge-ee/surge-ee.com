5cc3bb2f65f1025cb99b522f5429c87c0da3fc44
russo <mike@digitalsurgeons.com>
2012-11-09 13:31:58 -0800

    resizing the panel a fonts size

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function() {
-	
+
+	fontResize();
+
+	function fontResize() {
+		var	$pTxt = $('.panel a'),
+			pWidth = $pTxt.parent().width(),
+			newSize = pWidth / 70;
+		$pTxt.css('font-size', newSize + 'rem');
+	}
+
+	$(window).resize(fontResize);
+
 });
